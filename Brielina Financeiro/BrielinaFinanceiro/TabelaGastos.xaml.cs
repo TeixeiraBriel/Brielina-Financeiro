0817155a97b83566925702f8095ab4be426bc0f8
Gabriel Teixeira <biel.gt@gmail.com>
2021-10-04 18:28:59 -0300

    Limpando Codigo

--- Brielina Financeiro/BrielinaFinanceiro/TabelaGastos.xaml.cs
+++ Brielina Financeiro/BrielinaFinanceiro/TabelaGastos.xaml.cs
@@ -20,51 +20,9 @@ namespace BrielinaFinanceiro
     /// </summary>
     public partial class TabelaGastos : Page
     {
-        private bool firstA = true;
-        private bool firstB = true;
-        private bool firstC = true;
-        private bool firstD = true;
-
         public TabelaGastos()
         {
             InitializeComponent();
-        }
-
-        private void DataDiferenteFunc(object sender, RoutedEventArgs e)
-        {
-            if (firstA)
-            {
-                firstA = false;
-                return;
-            }
-            if (DataDiferenteRadio.IsChecked == true)
-            {
-                DataInseridoPanel.Visibility = Visibility.Collapsed;
-            }
-            else
-            {
-                DataInseridoPanel.Visibility = Visibility.Visible;
-            }
-        }
-
-        private void GastoFixoRadioFunc(object sender, RoutedEventArgs e)
-        {
-            if (firstB)
-            {
-                firstB = false;
-                return;
-            }
-            if (GastoFixoRadio.IsChecked == true)
-            {
-                gastoFixoPanel.Visibility = Visibility.Visible;
-                DataVencimentoPanel.Visibility = Visibility.Visible;
-            }
-            else
-            {
-                gastoFixoPanel.Visibility = Visibility.Collapsed;
-                DataVencimentoPanel.Visibility = Visibility.Collapsed;
-            }
-
         }
     }
 }
