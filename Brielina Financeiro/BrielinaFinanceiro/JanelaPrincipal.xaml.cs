69c40c69b420a7c7ebbdb9bb8e0d09b4d201650b
Gabriel Teixeira <biel.gt@gmail.com>
2021-10-04 15:36:18 -0300

    Commit Base

---
+++ Brielina Financeiro/BrielinaFinanceiro/JanelaPrincipal.xaml.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace BrielinaFinanceiro
+{
+    public partial class JanelaPrincipal : Window
+    {
+        private bool collapsed = true;
+        private SideMenu menu;
+
+        public JanelaPrincipal()
+        {
+            InitializeComponent();
+            menu = new SideMenu(this);
+
+            this.sidebarMenuFrame.NavigationService.Navigate(menu);
+        }
+
+        private void FecharApp(object sender, RoutedEventArgs e)
+        {
+            this.Close();
+        }
+
+        private void MoverTela(object sender, MouseButtonEventArgs e)
+        {
+            if (e.LeftButton == MouseButtonState.Pressed)
+            {
+                DragMove();
+            }
+        }
+
+        private void Collapse(object sender, MouseButtonEventArgs e)
+        {
+            if (sidebarMenuFrame.Visibility == Visibility.Hidden)
+            {
+                sidebarMenuFrame.Visibility = Visibility.Visible;
+                CenterFrame.Margin = new Thickness(0, 0, 0, 0);
+                btnCollapse.Visibility = Visibility.Hidden;
+            }
+            else
+            {
+                btnCollapse.Visibility = Visibility.Visible;
+                sidebarMenuFrame.Visibility = Visibility.Hidden;
+                CenterFrame.Margin = new Thickness(-150, 0, 0, 0);
+            }
+        }
+
+        private void Inicio(object sender, MouseButtonEventArgs e)
+        {
+            CenterFrame.Source = new Uri("Inicio.xaml", UriKind.Relative);
+        }
+
+        private void CadastroDeGasto(object sender, MouseButtonEventArgs e)
+        {
+            CenterFrame.Source = new Uri("CadastroDeGasto.xaml", UriKind.Relative);
+        }
+
+        private void Sobre(object sender, MouseButtonEventArgs e)
+        {
+            CenterFrame.Source = new Uri("Page0.xaml", UriKind.Relative);
+        }
+    }
+}
---
+++ Brielina Financeiro/BrielinaFinanceiro/SideMenu.xaml.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace BrielinaFinanceiro
+{
+    /// <summary>
+    /// Interação lógica para SideMenu.xam
+    /// </summary>
+    public partial class SideMenu : Page
+    {
+        private JanelaPrincipal janelaPrincipal;
+        public SideMenu(JanelaPrincipal JanelaPrincipal)
+        {
+            InitializeComponent();
+            janelaPrincipal = JanelaPrincipal;
+        }
+
+        private void Collapse(object sender, RoutedEventArgs e)
+        {
+            if (janelaPrincipal.sidebarMenuFrame.Visibility == Visibility.Hidden)
+            {
+                janelaPrincipal.sidebarMenuFrame.Visibility = Visibility.Visible;
+                janelaPrincipal.CenterFrame.Margin = new Thickness(0, 0, 0, 0);
+                janelaPrincipal.btnCollapse.Visibility = Visibility.Hidden;
+            }
+            else
+            {
+                janelaPrincipal.btnCollapse.Visibility = Visibility.Visible;
+                janelaPrincipal.sidebarMenuFrame.Visibility = Visibility.Hidden;
+                janelaPrincipal.CenterFrame.Margin = new Thickness(-150, 0, 0, 0);
+            }
+        }
+
+        private void Cadastrar(object sender, RoutedEventArgs e)
+        {
+
+        }
+    }
+}
